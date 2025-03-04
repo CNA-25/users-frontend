3eab8389604ff6c56adb912ce6cc4469257d6bd6
Rasmus Räihä <68077022+Ranzz02@users.noreply.github.com>
2025-03-04 17:04:27 +0200

    change orders styling & add orders button

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,56 +2,63 @@ import { useAuthStore } from "@/stores/auth";
 import { Link } from "react-router-dom";
 
 const Navbar: React.FC = () => {
-  const { authenticated, setAuthenticated, setToken } = useAuthStore();
+	const { authenticated, setAuthenticated, setToken } = useAuthStore();
 
-  const handleLogout = () => {
-    setAuthenticated(false);
-    setToken(undefined);
-  };
+	const handleLogout = () => {
+		setAuthenticated(false);
+		setToken(undefined);
+	};
 
-  return (
-    <div className="w-full bg-black">
-      <div className="flex flex-col max-w-screen-xl p-2 mx-auto md:p-4 md:flex-row">
-        <div className="flex flex-row items-center justify-between flex-1">
-          <h1 className="text-3xl font-bold text-orange-500">BeerCraft</h1>
-        </div>
-        <div className="flex flex-col justify-end flex-1 md:flex-row">
-          {authenticated ? (
-            <>
-              <Link
-                to="/edit"
-                className="mx-4 text-2xl text-orange-500 md:text-md hover:text-orange-800"
-              >
-                Profile
-              </Link>
-              <Link
-                className="mx-4 text-2xl text-orange-500 md:text-md hover:text-orange-800"
-                onClick={handleLogout}
-                to="/"
-              >
-                Logout
-              </Link>
-            </>
-          ) : (
-            <>
-              <Link
-                to="/"
-                className="mx-4 text-2xl text-orange-500 md:text-md hover:text-orange-800"
-              >
-                Login
-              </Link>
-              <Link
-                to="/register"
-                className="mx-4 text-2xl text-orange-500 md:text-md hover:text-orange-800"
-              >
-                Register
-              </Link>
-            </>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="w-full bg-black">
+			<div className="flex flex-col max-w-screen-xl p-2 mx-auto md:p-4 md:flex-row">
+				<div className="flex flex-row items-center justify-between flex-1">
+					<h1 className="text-3xl font-bold text-orange-500">BeerCraft</h1>
+				</div>
+				<div className="flex flex-col justify-end flex-1 md:flex-row">
+					{authenticated ? (
+						<>
+							<Link
+								to="/edit"
+								className="mx-4 text-2xl text-orange-500 md:text-md hover:text-orange-800"
+							>
+								Profile
+							</Link>
+
+							<Link
+								to="/orders"
+								className="mx-4 text-2xl text-orange-500 md:text-md hover:text-orange-800"
+							>
+								My Orders
+							</Link>
+							<Link
+								className="mx-4 text-2xl text-orange-500 md:text-md hover:text-orange-800"
+								onClick={handleLogout}
+								to="/"
+							>
+								Logout
+							</Link>
+						</>
+					) : (
+						<>
+							<Link
+								to="/"
+								className="mx-4 text-2xl text-orange-500 md:text-md hover:text-orange-800"
+							>
+								Login
+							</Link>
+							<Link
+								to="/register"
+								className="mx-4 text-2xl text-orange-500 md:text-md hover:text-orange-800"
+							>
+								Register
+							</Link>
+						</>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
