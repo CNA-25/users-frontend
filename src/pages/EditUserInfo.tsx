493f448e26abcc5f30af60e6c2fd0587d2c9e715
Stenoliv <stenbaco@arcada.fi>
2025-03-04 23:48:18 +0200

    Visual fix to edituser

--- src/pages/EditUserInfo.tsx
+++ src/pages/EditUserInfo.tsx
@@ -144,6 +144,8 @@ const EditUserInfo: React.FC = () => {
             <h1 className="my-8 text-6xl font-bold text-center text-orange-500">
               Update your information
             </h1>
+            <h4>A * above a field means it is required</h4>
+            <h2 className="text-xl font-bold text-orange-500">*</h2>
             <input
               type="text"
               name="name"
@@ -153,7 +155,6 @@ const EditUserInfo: React.FC = () => {
               required
               className="p-2 text-orange-200 bg-black border border-black rounded"
             />
-            <h2>*</h2>
             <input
               type="email"
               name="email"
@@ -163,7 +164,7 @@ const EditUserInfo: React.FC = () => {
               required
               className="p-2 text-orange-200 bg-black border border-black rounded"
             />
-            <h2>*</h2>
+            <h2 className="text-xl font-bold text-orange-500">*</h2>
             <input
               type="tel"
               name="phone"
@@ -173,6 +174,7 @@ const EditUserInfo: React.FC = () => {
               required
               className="p-2 text-orange-200 bg-black border border-black rounded"
             />
+            <h2 className="text-xl font-bold text-orange-500">*</h2>
             <input
               type="date"
               name="birthday"
@@ -182,7 +184,6 @@ const EditUserInfo: React.FC = () => {
               required
               className="p-2 text-orange-200 bg-black border border-black rounded"
             />
-            <h2>*</h2>
             <input
               type="password"
               name="password"
