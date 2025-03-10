e8fa0dc2dd7ee0c3f2aeec173676e6d1adce9791
Stenoliv <stenbaco@arcada.fi>
2025-03-10 23:44:34 +0200

    fix build fail

--- src/App.tsx
+++ src/App.tsx
@@ -3,40 +3,38 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MyOrdersPage from "./pages/MyOrdersPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegistrationPage";
-import ResetPasswordPage from "./pages/ResetPasswordPage";
 import EditUserInfo from "./pages/EditUserInfo";
 import ProtectedPage from "./components/ProtectedPage";
 import { ToastContainer } from "react-toastify";
 
 const App: React.FC = () => {
-	return (
-		<Router>
-			<ToastContainer theme="dark" />
-			<main className="flex flex-col w-screen h-screen p-0 mx-0 bg-zinc-950">
-				<Routes>
-					<Route path="/" element={<LoginPage />} />
-					<Route path="/register" element={<RegisterPage />} />
-					<Route path="/reset" element={<ResetPasswordPage />} />
-					<Route
-						path="/edit"
-						element={
-							<ProtectedPage>
-								<EditUserInfo />
-							</ProtectedPage>
-						}
-					/>
-					<Route
-						path="/orders"
-						element={
-							<ProtectedPage>
-								<MyOrdersPage />
-							</ProtectedPage>
-						}
-					/>
-				</Routes>
-			</main>
-		</Router>
-	);
+  return (
+    <Router>
+      <ToastContainer theme="dark" />
+      <main className="flex flex-col w-screen h-screen p-0 mx-0 bg-zinc-950">
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route
+            path="/edit"
+            element={
+              <ProtectedPage>
+                <EditUserInfo />
+              </ProtectedPage>
+            }
+          />
+          <Route
+            path="/orders"
+            element={
+              <ProtectedPage>
+                <MyOrdersPage />
+              </ProtectedPage>
+            }
+          />
+        </Routes>
+      </main>
+    </Router>
+  );
 };
 
 export default App;
